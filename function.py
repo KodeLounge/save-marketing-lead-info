8c86f2879a9952714313479beda8f7fdb5a6a9a7
Prateek Choudhary <prateekkish@gmail.com>
2019-05-16 17:15:12 +0530

    Send email to KL employees when contact us form is submitted

--- function.py
+++ function.py
@@ -8,95 +8,98 @@ from botocore.exceptions import ClientError
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def save_marketing_lead_info(event, context):
 
+    dynamodb = boto3.resource('dynamodb')
+    leads_table = dynamodb.Table('kl-marketing-leads')
+    request_params = json.loads(event["body"])
 
-  dynamodb = boto3.resource('dynamodb')
-  leads_table = dynamodb.Table('kl-marketing-leads')
-  request_params = json.loads(event["body"])
+    with leads_table.batch_writer() as batch:
+        batch.put_item(Item=lead_object_from_params(
+            request_params["marketing_lead"]))
 
-  with leads_table.batch_writer() as batch:
-    batch.put_item(Item=lead_object_from_params(request_params["marketing_lead"]))
-  
-  send_mail(request_params["marketing_lead"])
-  
-  return {
-    "isBase64Encoded": "true",
-    "statusCode": 200,
-    "headers" : {
-      "Access-Control-Allow-Origin": os.environ.get("MARK_REQUEST_ORIGIN") or "*"
-    },
-    "body": json.dumps({'message':'Successfully saved.'})
-  }
+    send_mail(request_params["marketing_lead"])
+
+    return {
+        "isBase64Encoded": "true",
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": os.environ.get("MARK_REQUEST_ORIGIN") or "*"
+        },
+        "body": json.dumps({'message': 'Successfully saved.'})
+    }
 
 
 def send_mail(context):
-  SENDER = os.environ.get('SENDER_SUPPORT_EMAIL') or "KL Marketing Enquiries <enquiries@kodelounge.com>"
-  AWS_REGION = "us-west-2"
-  SUBJECT = "New Lead from KL Marketing"
-  CHARSET = 'UTF-8'
-  recipients = os.environ.get('RECEIVER_EMAIL') or "sujit@kodelounge.com"
-  recipients = recipients.split(',')
-  recipients = [r.strip() for r in recipients]
+    SENDER = os.environ.get(
+        'SENDER_SUPPORT_EMAIL') or "KL Marketing Enquiries <enquiries@kodelounge.com>"
+    AWS_REGION = "us-west-2"
+    SUBJECT = "New Lead from KL Marketing"
+    CHARSET = 'UTF-8'
+    recipients = os.environ.get('RECEIVER_EMAIL') or "sujit@kodelounge.com"
+    recipients = recipients.split(',')
+    recipients = [r.strip() for r in recipients]
 
-  mail_body_params = {
-    'name': context['name'],
-    'subject': SUBJECT
-  }
-  
-  if 'email' in context:
-    mail_body_params['email'] = context['email']
-  if 'phone' in context:
-    mail_body_params['phone'] = context['phone']
-  if 'message' in context:
-    mail_body_params['message'] = context['message']
+    mail_body_params = {
+        'name': context['name'],
+        'subject': context['subject'],
+    }
 
-  BODY_HTML = email_content(mail_body_params)
+    if 'email' in context:
+        mail_body_params['email'] = context['email']
+    if 'phone' in context:
+        mail_body_params['phone'] = context['phone']
+    if 'message' in context:
+        mail_body_params['message'] = context['message']
 
-  client = boto3.client('ses',region_name=AWS_REGION)
+    BODY_HTML = email_content(mail_body_params)
 
-  try:
-    response = client.send_email(
-        Destination={
-            'ToAddresses': recipients
-        },
-        Message={
-            'Body': {
-                'Html': {
-                    'Charset': CHARSET,
-                    'Data': BODY_HTML,
+    client = boto3.client('ses', region_name=AWS_REGION)
+
+    try:
+        response = client.send_email(
+            Destination={
+                'ToAddresses': recipients
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': "Mail Text Here TODO",
+                    },
                 },
-                'Text': {
+                'Subject': {
                     'Charset': CHARSET,
-                    'Data': "Mail Text Here TODO",
+                    'Data': SUBJECT,
                 },
             },
-            'Subject': {
-                'Charset': CHARSET,
-                'Data': SUBJECT,
-            },
-        },
-        Source=SENDER
-    )
-  # Display an error if something goes wrong.	
-  except ClientError as e:
-      print(e.response['Error']['Message'])
-  else:
-      print("Email sent! Message ID:"),
-      print(response['MessageId'])
+            Source=SENDER
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
 
 
 def lead_object_from_params(lead_params):
-  passed_parameters = set(lead_params.keys())
-  allowed_keys = {'name','email','phone','message'}
-  permitted_keys = passed_parameters & allowed_keys
-  lead_params = {param : lead_params[param] for param in permitted_keys if lead_params[param] is not '' }
-  lead_params["id"] = str(uuid.uuid4())
-  return lead_params
+    passed_parameters = set(lead_params.keys())
+    allowed_keys = {'name', 'email', 'phone', 'message'}
+    permitted_keys = passed_parameters & allowed_keys
+    lead_params = {param: lead_params[param]
+                   for param in permitted_keys if lead_params[param] is not ''}
+    lead_params["id"] = str(uuid.uuid4())
+    return lead_params
 
 
 def email_content(params):
-  str = '''\
+    str = '''\
   <!DOCTYPE html>
     <html>
     <head>
@@ -114,15 +117,16 @@ def email_content(params):
           Name: {name}.<br>
   '''
 
-  if 'email' in params and params['email'] is not '' :
-      str += 'Email Address: <a href="mailto:{email}">{email}</a><br>'
-  else :
-      str += 'Phone: {phone}'
+    if 'email' in params and params['email'] is not '':
+        str += 'Email Address: <a href="mailto:{email}">{email}</a><br>'
 
-  if 'message' in params and params['message'] is not '' :
-      str += 'Message: {message}'
+    if 'subject' in params and params['subject'] is not '':
+        str += 'Subject: {subject}<br>'
 
-  str+= ''' 
+    if 'message' in params and params['message'] is not '':
+        str += 'Message: <br> {message}'
+
+    str += ''' 
         </div>
         <div style="font-size: 16px;  margin-top:13px;">Thanks</div>
       </div>
@@ -130,5 +134,4 @@ def email_content(params):
   </html>
   '''
 
-  return str.format(**params)
-
+    return str.format(**params)
